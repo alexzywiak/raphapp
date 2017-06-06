b0debde492d6d7704848941403c4d0ddae0ad34e
Alex Zywiak <alex.zywiak@gmail.com>
2017-06-05 22:06:33 -0600

    init commit

---
+++ chicken.js
@@ -0,0 +1,18 @@
+var request = require('request');
+
+var quoteUrl = 'https://api.forismatic.com/api/1.0/\?method\=getQuote\&lang\=en\&format\=json';
+
+module.exports = {
+  getQuote: function(cb){
+    return request(quoteUrl, function(e, r, b) {
+      if(e) {
+        cb(error);
+      }
+      cb(null, JSON.parse(b.replace("\\", "").replace("'", " ")));
+    });
+  },
+
+  generateNumber: function() {
+    return Math.floor(Math.random() * 10);
+  }
+}
